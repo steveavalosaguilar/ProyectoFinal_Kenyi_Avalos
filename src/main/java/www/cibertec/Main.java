772420fc37a161bdad37cf610e0a6210e3a57e23
Steven Avalos Aguilar <steveavalosaguilar@gmail.com>
2026-08-21 20:31:30 -0500

    Ejercicio realizado del proyecto final

--- src/main/java/www/cibertec/Main.java
+++ src/main/java/www/cibertec/Main.java
@@ -1,17 +1,102 @@
 package www.cibertec;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import javax.swing.JOptionPane;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+
+    // Metodo calcular edad
+    public int calcularEdad(int anio) {
+        return 2026 - anio;
+    }
+
+    // Metodo calcular peso total
+    public double calcularPesoTotal(double peso, int cantidad) {
+        return peso * cantidad;
+    }
+
+    public void main(String[] args) {
+
+        String nombre;
+        int anio;
+        double peso;
+        boolean garantia;
+
+        nombre = JOptionPane.showInputDialog("Ingrese nombre y apellidos:");
+
+        anio = Integer.parseInt(
+                JOptionPane.showInputDialog("Ingrese año de nacimiento:"));
+
+        peso = Double.parseDouble(
+                JOptionPane.showInputDialog("Ingrese peso en kg:"));
+
+        garantia = Boolean.parseBoolean(
+                JOptionPane.showInputDialog("¿Desea garantia? true/false"));
+
+        int edad = calcularEdad(anio);
+
+        // IF / ELSE
+        if (edad >= 18) {
+            System.out.println("Es mayor de edad.");
+        } else {
+            System.out.println("Es menor de edad.");
         }
+
+        // SWITCH
+        String opcion = JOptionPane.showInputDialog(
+                "Seleccione un modelo:\n" +
+                        "1. Toyota Hilux\n" +
+                        "2. Toyota Hiace\n" +
+                        "3. Kia Seltos");
+
+        String modelo;
+
+        switch (opcion) {
+            case "1":
+                modelo = "Toyota Hilux";
+                break;
+            case "2":
+                modelo = "Toyota Hiace";
+                break;
+            case "3":
+                modelo = "Kia Seltos";
+                break;
+            default:
+                modelo = "No seleccionado";
+        }
+
+        // WHILE
+        int cantidad = 0;
+
+        while (cantidad <= 0) {
+            cantidad = Integer.parseInt(
+                    JOptionPane.showInputDialog("Ingrese cantidad:"));
+        }
+
+        // FOR
+        System.out.println("\nUnidades:");
+
+        for (int i = 1; i <= cantidad; i++) {
+            System.out.println("Unidad " + i + ": " + modelo);
+        }
+
+        // DO WHILE
+        String confirmar;
+
+        do {
+            confirmar = JOptionPane.showInputDialog(
+                    "¿Confirmar compra? si/no");
+        } while (!confirmar.equalsIgnoreCase("si")
+                && !confirmar.equalsIgnoreCase("no"));
+
+        double pesoTotal = calcularPesoTotal(peso, cantidad);
+
+        System.out.println("\n= RESULTADO =");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Cantidad: " + cantidad);
+        System.out.println("Peso total: " + pesoTotal + " kg");
+        System.out.println("Garantia: " + garantia);
+        System.out.println("Compra confirmada.");
     }
 }
